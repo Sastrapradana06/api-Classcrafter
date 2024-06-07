6589ae08e8a5891d16754b64418b95223264dab2
Sastrapradana06 <saatrapradana@gmail.com>
2024-06-07 22:51:03 +0700

    api updatePassword

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,6 @@
 const supabase = require("../lib/supabase");
 const jwt = require("jsonwebtoken");
-const { comparePassword } = require("../utils");
+const { comparePassword, hashPassword } = require("../utils");
 require("dotenv").config();
 
 const login = async (req, res) => {
@@ -49,4 +49,50 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { login };
+const updatePassword = async (req, res) => {
+  const { idUser, password_lama, password_baru } = req.body;
+  try {
+    const { data, error } = await supabase
+      .from("data-siswa")
+      .select()
+      .eq("id", idUser);
+    if (error) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Maaf, terjadi kesalahan" });
+    }
+
+    if (data.length === 0) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Gagal update, user tidak ditemukan" });
+    }
+    const userData = data[0];
+
+    const match = await comparePassword(password_lama, userData.password);
+    if (!match) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Gagal update, password lama salah" });
+    } else {
+      const newPassword = await hashPassword(password_baru);
+      const { error } = await supabase
+        .from("data-siswa")
+        .update({ password: newPassword })
+        .eq("id", idUser);
+      if (error) {
+        return res
+          .status(404)
+          .json({ status: false, message: "Maaf, terjadi kesalahan" });
+      }
+    }
+    return res
+      .status(200)
+      .json({ status: true, message: "Berhasil mengubah password" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ status: false, message: error });
+  }
+};
+
+module.exports = { login, updatePassword };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { login } = require("../controller/authController");
+const { login, updatePassword } = require("../controller/authController");
 
 router.post("/login", login);
+router.post("/update-password", updatePassword);
 
 module.exports = router;
